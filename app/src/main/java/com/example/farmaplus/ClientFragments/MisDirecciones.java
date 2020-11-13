5f9d86a95d9abe165b9192afb6d1982effa068cc
oscar2706 <spat_2706@outlook.com>
2020-11-13 15:12:57 -0600

    Agregado menu actionVar, Agregado ui misDirecciones

--- app/src/main/java/com/example/farmaplus/ClientFragments/MisDirecciones.java
+++ app/src/main/java/com/example/farmaplus/ClientFragments/MisDirecciones.java
@@ -10,9 +10,9 @@ import android.view.ViewGroup;
 
 import com.example.farmaplus.R;
 
-public class Configuracion extends Fragment {
+public class MisDirecciones extends Fragment {
 
-    public Configuracion() {
+    public MisDirecciones() {
         // Required empty public constructor
     }
 
@@ -24,6 +24,6 @@ public class Configuracion extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_configuracion, container, false);
+        return inflater.inflate(R.layout.fragment_mis_direcciones, container, false);
     }
 }
--- app/src/main/java/com/example/farmaplus/MainActivity.java
+++ app/src/main/java/com/example/farmaplus/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.farmaplus;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -10,6 +12,10 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         navController = Navigation.findNavController(this, R.id.fragment_navigation);
         NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_direcciones:
+                navController.navigate(R.id.action_principalCliente_to_configuracion);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
