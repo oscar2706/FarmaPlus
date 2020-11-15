b01d4f81fc195a855ae83a63b9660d6eefaafef2
oscar2706 <spat_2706@outlook.com>
2020-11-15 06:26:15 -0600

    Modificado CustomToast (mensaje personalizado), Agregado ilustracion pedido en camino

--- app/src/main/java/com/example/farmaplus/client/CustomToast.java
+++ app/src/main/java/com/example/farmaplus/client/CustomToast.java
@@ -5,6 +5,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.farmaplus.R;
@@ -13,11 +14,13 @@ import java.util.zip.Inflater;
 
 public class CustomToast {
 
-    public static void showOkToast(Activity activity){
+    public static void showOkToast(Activity activity, String mensaje){
         LayoutInflater inflater = activity.getLayoutInflater();
         View toast_layout = inflater.inflate(R.layout.toast_pedido_realizado,
                 (ViewGroup)activity.findViewById(R.id.toast_ok));
         final Toast toast = new Toast(activity.getApplicationContext());
+        TextView textView = toast_layout.findViewById(R.id.textView_toastOk);
+        textView.setText(mensaje);
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(toast_layout);
