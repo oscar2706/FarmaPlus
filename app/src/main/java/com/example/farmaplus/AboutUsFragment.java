aafd0264ef1c56a2fac4f981f1528f0aadd5d6a6
oscar2706 <spat_2706@outlook.com>
2020-11-20 13:58:40 -0600

    Agregado AboutUsFragment, Modificado ui fragment_mis_direcciones (not long)

---
+++ app/src/main/java/com/example/farmaplus/AboutUsFragment.java
@@ -0,0 +1,27 @@
+package com.example.farmaplus;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutUsFragment extends Fragment {
+
+    public AboutUsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        return view;
+    }
+}
