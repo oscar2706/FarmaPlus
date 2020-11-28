90de6ea8221e109ef4f379d5814ad3c9def8ddae
oscar2706 <spat_2706@outlook.com>
2020-11-27 20:47:37 -0600

    Agregado bottomDialogConfirmaNuevoMedicamento

---
+++ app/src/main/java/com/example/farmaplus/pharmacy/BottomDialogConfirmaMedicamento.java
@@ -0,0 +1,25 @@
+package com.example.farmaplus.pharmacy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.farmaplus.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomDialogConfirmaMedicamento extends BottomSheetDialogFragment {
+    public BottomDialogConfirmaMedicamento() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_dialog_confirma_medicamento, container, false);
+        return view;
+    }
+}
+
--- app/src/main/java/com/example/farmaplus/pharmacy/NuevoMedicamentoFragment.java
+++ app/src/main/java/com/example/farmaplus/pharmacy/NuevoMedicamentoFragment.java
@@ -2,15 +2,23 @@ package com.example.farmaplus.pharmacy;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 
 import com.example.farmaplus.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class NuevoMedicamentoFragment extends Fragment {
+public class NuevoMedicamentoFragment extends Fragment implements View.OnClickListener {
+    NavController navController;
 
     public NuevoMedicamentoFragment() {
     }
@@ -24,6 +32,24 @@ public class NuevoMedicamentoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.farmacia_nuevo_medicamento, container, false);
+        Button button_registrarMedicamento = view.findViewById(R.id.buttonRegistrarMedicamento);
+        button_registrarMedicamento.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        Navigation.setViewNavController(view, navController);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonRegistrarMedicamento:
+                navController.navigate(R.id.action_nuevoMedicamento_to_bottomDialogConfirmaMedicamento);
+                break;
+        }
     }
 }
